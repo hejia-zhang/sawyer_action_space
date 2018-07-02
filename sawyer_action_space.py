1f26f84177c28c8583bc9dc3f9ff36a956ed2db7
Hejia Zhang <hjzh578@gmail.com>
2018-07-01 23:34:26 -0700

    fist commit

---
+++ sawyer_action_space.py
@@ -0,0 +1,47 @@
+import rospy
+
+from sensor_msgs.msg import JointState
+
+
+sawyer_velocity_action_space = {}
+sawyer_effort_action_space = {}
+
+
+def on_msg(data):
+    joints = data.name
+    velocities = data.velocity
+    efforts = data.effort
+
+    idx = 0
+    for joint in joints:
+        velocity = velocities[idx]
+        effort = efforts[idx]
+        idx += 1
+
+        if joint not in sawyer_velocity_action_space:
+            sawyer_velocity_action_space[joint] = float(f'{abs(velocity):.4f}')
+        else:
+            if abs(velocity) > sawyer_velocity_action_space[joint]:
+                sawyer_velocity_action_space[joint] = float(f'{abs(velocity):.4f}')
+
+        if joint not in sawyer_effort_action_space:
+            sawyer_effort_action_space[joint] = float(f'{abs(effort):.4f}')
+        else:
+            if abs(effort) > sawyer_effort_action_space[joint]:
+                sawyer_effort_action_space[joint] = float(f'{abs(effort):.4f}')
+
+
+def run():
+    rospy.init_node('sawyer_action_space')
+    rospy.Subscriber("/robot/joint_states", JointState, on_msg)
+    rate = rospy.Rate(100)
+    try:
+        while not rospy.is_shutdown():
+            rate.sleep()
+    except:
+        print('The velocity action space is: ', sawyer_velocity_action_space)
+        print('The effort action space is: ', sawyer_effort_action_space)
+
+
+if __name__ == '__main__':
+    run()
